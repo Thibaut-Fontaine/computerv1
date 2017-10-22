7b8fcf34ccae366262942e318f3bb1df95ab3730
Thibaut FONTAINE <tfontain@e2r9p15.42.fr>
2017-10-22 19:12:23 +0200

    init files

---
+++ calculate.js
@@ -0,0 +1,15 @@
+#!/usr/local/bin/node
+if (process.argv.length != 3)
+	process.exit();
+var str = process.argv[2];
+//var i;
+//while (str[i] != '^')
+//	++i;
+str = str.split(" ");
+str.forEach(function (item) {
+	
+});
+
+process.stdout.write("Reduced form: " + "\n");
+process.stdout.write("Polynomial degree: " + "\n");
+process.stdout.write("");
